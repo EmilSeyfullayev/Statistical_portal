515d8fddee9bd41e93eb6db124c853b8625c1921
adm_emil <adm_emil@users.noreply.github.com>
2026-06-16 13:23:08 +0000

    Add English and Russian goods nomenclature data

---
+++ apps/imports/services/goods_nomenclature_short_English.py
@@ -0,0 +1,496 @@
+# -*- coding: utf-8 -*-
+
+# English shortened version of goods_nomenclature_short.py
+# Values are shortened to key terms for dashboard/report usage.
+
+goods_nomenclature_short_english_data = [
+    {
+        "section": "Live animals and animal products",
+        "categories": [
+            {
+                "code": "01",
+                "name": "Live animals"
+            },
+            {
+                "code": "02",
+                "name": "Meat products"
+            },
+            {
+                "code": "03",
+                "name": "Fish and aquatic products"
+            },
+            {
+                "code": "04",
+                "name": "Dairy, eggs and honey"
+            },
+            {
+                "code": "05",
+                "name": "Other animal products"
+            }
+        ]
+    },
+    {
+        "section": "Vegetable products",
+        "categories": [
+            {
+                "code": "06",
+                "name": "Live plants and flowers"
+            },
+            {
+                "code": "07",
+                "name": "Vegetables and root crops"
+            },
+            {
+                "code": "08",
+                "name": "Fruits and nuts"
+            },
+            {
+                "code": "09",
+                "name": "Coffee, tea and spices"
+            },
+            {
+                "code": "10",
+                "name": "Cereals"
+            },
+            {
+                "code": "11",
+                "name": "Milling products"
+            },
+            {
+                "code": "12",
+                "name": "Oil seeds and fodder"
+            },
+            {
+                "code": "13",
+                "name": "Resins and plant extracts"
+            },
+            {
+                "code": "14",
+                "name": "Vegetable plaiting materials"
+            }
+        ]
+    },
+    {
+        "section": "Fats, oils and waxes",
+        "categories": [
+            {
+                "code": "15",
+                "name": "Fats, oils and waxes"
+            }
+        ]
+    },
+    {
+        "section": "Prepared foods and beverages",
+        "categories": [
+            {
+                "code": "16",
+                "name": "Meat and fish products"
+            },
+            {
+                "code": "17",
+                "name": "Sugar and confectionery"
+            },
+            {
+                "code": "18",
+                "name": "Cocoa products"
+            },
+            {
+                "code": "19",
+                "name": "Flour and cereal products"
+            },
+            {
+                "code": "20",
+                "name": "Fruit and vegetable products"
+            },
+            {
+                "code": "21",
+                "name": "Miscellaneous food products"
+            },
+            {
+                "code": "22",
+                "name": "Beverages and vinegar"
+            },
+            {
+                "code": "23",
+                "name": "Food waste and animal feed"
+            },
+            {
+                "code": "24",
+                "name": "Tobacco products"
+            }
+        ]
+    },
+    {
+        "section": "Mineral products",
+        "categories": [
+            {
+                "code": "25",
+                "name": "Salt, stone and cement"
+            },
+            {
+                "code": "26",
+                "name": "Ores and slag"
+            },
+            {
+                "code": "27",
+                "name": "Mineral fuels and oils"
+            }
+        ]
+    },
+    {
+        "section": "Chemical industry products",
+        "categories": [
+            {
+                "code": "28",
+                "name": "Inorganic chemicals"
+            },
+            {
+                "code": "29",
+                "name": "Organic chemical compounds"
+            },
+            {
+                "code": "30",
+                "name": "Pharmaceutical products"
+            },
+            {
+                "code": "31",
+                "name": "Fertilizers"
+            },
+            {
+                "code": "32",
+                "name": "Paints and varnishes"
+            },
+            {
+                "code": "33",
+                "name": "Perfumery and cosmetics"
+            },
+            {
+                "code": "34",
+                "name": "Soap and detergents"
+            },
+            {
+                "code": "35",
+                "name": "Proteins, starches and adhesives"
+            },
+            {
+                "code": "36",
+                "name": "Explosives and pyrotechnics"
+            },
+            {
+                "code": "37",
+                "name": "Photo and cinema goods"
+            },
+            {
+                "code": "38",
+                "name": "Miscellaneous chemical products"
+            }
+        ]
+    },
+    {
+        "section": "Plastic and rubber products",
+        "categories": [
+            {
+                "code": "39",
+                "name": "Plastic articles"
+            },
+            {
+                "code": "40",
+                "name": "Rubber articles"
+            }
+        ]
+    },
+    {
+        "section": "Leather, fur and bags",
+        "categories": [
+            {
+                "code": "41",
+                "name": "Raw and tanned hides"
+            },
+            {
+                "code": "42",
+                "name": "Leather and travel goods"
+            },
+            {
+                "code": "43",
+                "name": "Fur articles"
+            }
+        ]
+    },
+    {
+        "section": "Wood and plaiting products",
+        "categories": [
+            {
+                "code": "44",
+                "name": "Wood and charcoal"
+            },
+            {
+                "code": "45",
+                "name": "Cork articles"
+            },
+            {
+                "code": "46",
+                "name": "Plaiting articles"
+            }
+        ]
+    },
+    {
+        "section": "Paper and cardboard products",
+        "categories": [
+            {
+                "code": "47",
+                "name": "Pulp and waste paper"
+            },
+            {
+                "code": "48",
+                "name": "Paper and cardboard"
+            },
+            {
+                "code": "49",
+                "name": "Printed products"
+            }
+        ]
+    },
+    {
+        "section": "Textile products",
+        "categories": [
+            {
+                "code": "50",
+                "name": "Silk"
+            },
+            {
+                "code": "51",
+                "name": "Wool and animal hair"
+            },
+            {
+                "code": "52",
+                "name": "Cotton"
+            },
+            {
+                "code": "53",
+                "name": "Vegetable textile fibers"
+            },
+            {
+                "code": "54",
+                "name": "Man-made and synthetic filaments"
+            },
+            {
+                "code": "55",
+                "name": "Synthetic staple fibers"
+            },
+            {
+                "code": "56",
+                "name": "Felt, yarn and rope"
+            },
+            {
+                "code": "57",
+                "name": "Carpets"
+            },
+            {
+                "code": "58",
+                "name": "Special fabrics and lace"
+            },
+            {
+                "code": "59",
+                "name": "Technical textile materials"
+            },
+            {
+                "code": "60",
+                "name": "Knitted fabrics"
+            },
+            {
+                "code": "61",
+                "name": "Knitted clothing"
+            },
+            {
+                "code": "62",
+                "name": "Non-knitted clothing"
+            },
+            {
+                "code": "63",
+                "name": "Made-up textile articles"
+            }
+        ]
+    },
+    {
+        "section": "Footwear and accessories",
+        "categories": [
+            {
+                "code": "64",
+                "name": "Footwear and parts"
+            },
+            {
+                "code": "65",
+                "name": "Headgear"
+            },
+            {
+                "code": "66",
+                "name": "Umbrellas and walking sticks"
+            },
+            {
+                "code": "67",
+                "name": "Feathers and artificial flowers"
+            }
+        ]
+    },
+    {
+        "section": "Stone, ceramics and glass",
+        "categories": [
+            {
+                "code": "68",
+                "name": "Stone and cement articles"
+            },
+            {
+                "code": "69",
+                "name": "Ceramic articles"
+            },
+            {
+                "code": "70",
+                "name": "Glass articles"
+            }
+        ]
+    },
+    {
+        "section": "Precious stones and metals",
+        "categories": [
+            {
+                "code": "71",
+                "name": "Jewelry and precious stones"
+            }
+        ]
+    },
+    {
+        "section": "Base metals",
+        "categories": [
+            {
+                "code": "72",
+                "name": "Ferrous metals"
+            },
+            {
+                "code": "73",
+                "name": "Ferrous metal articles"
+            },
+            {
+                "code": "74",
+                "name": "Copper articles"
+            },
+            {
+                "code": "75",
+                "name": "Nickel articles"
+            },
+            {
+                "code": "76",
+                "name": "Aluminum articles"
+            },
+            {
+                "code": "78",
+                "name": "Lead articles"
+            },
+            {
+                "code": "79",
+                "name": "Zinc articles"
+            },
+            {
+                "code": "80",
+                "name": "Tin articles"
+            },
+            {
+                "code": "81",
+                "name": "Other base metals"
+            },
+            {
+                "code": "82",
+                "name": "Metal tools and knives"
+            },
+            {
+                "code": "83",
+                "name": "Other metal articles"
+            }
+        ]
+    },
+    {
+        "section": "Machinery and electrical equipment",
+        "categories": [
+            {
+                "code": "84",
+                "name": "Mechanical equipment"
+            },
+            {
+                "code": "85",
+                "name": "Electrical equipment"
+            }
+        ]
+    },
+    {
+        "section": "Transport vehicles",
+        "categories": [
+            {
+                "code": "86",
+                "name": "Railway transport"
+            },
+            {
+                "code": "87",
+                "name": "Road vehicles"
+            },
+            {
+                "code": "88",
+                "name": "Aircraft"
+            },
+            {
+                "code": "89",
+                "name": "Ships and boats"
+            }
+        ]
+    },
+    {
+        "section": "Optical and medical instruments",
+        "categories": [
+            {
+                "code": "90",
+                "name": "Optical and medical instruments"
+            },
+            {
+                "code": "91",
+                "name": "Clocks and watches"
+            },
+            {
+                "code": "92",
+                "name": "Musical instruments"
+            }
+        ]
+    },
+    {
+        "section": "Arms, ammunition and accessories",
+        "categories": [
+            {
+                "code": "93",
+                "name": "Arms, ammunition and accessories"
+            }
+        ]
+    },
+    {
+        "section": "Miscellaneous industrial products",
+        "categories": [
+            {
+                "code": "94",
+                "name": "Furniture and lighting"
+            },
+            {
+                "code": "95",
+                "name": "Toys and sports goods"
+            },
+            {
+                "code": "96",
+                "name": "Miscellaneous manufactured articles"
+            }
+        ]
+    },
+    {
+        "section": "Art and antiques",
+        "categories": [
+            {
+                "code": "97",
+                "name": "Art and antiques"
+            }
+        ]
+    }
+]
